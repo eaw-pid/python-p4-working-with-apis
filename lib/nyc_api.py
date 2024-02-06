24f3a9be6f4f4b95b8e0cc07f3b8dab23612d2a4
Erin Wise <erinwise88@gmail.com>
2024-02-06 15:58:24 -0500

    complete

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,26 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  #   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://data.cityofnewyork.us/resource/dsg6-ifza.json"
 
     response = requests.get(URL)
     return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def programs_agencies(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      programs_list.append(program["centername"])
+
+    return programs_list
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+  
+programs = GetPrograms()
+agencies = programs.programs_agencies()
+
+##create a set to avoid duplicates!!
+for agency in set(agencies):
+  print(agency)
